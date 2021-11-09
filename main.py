6e60d5d00ffee7f355beee25c91fb4cc64a55f35
Vishaal Yalamanchali <44126511+vishytheswishy@users.noreply.github.com>
2021-11-08 16:20:53 -0800

    created barebones updates to flask api
    - peer programmed with kayla choi and frederick halo to create flask api and explained get and post method. Worked with Sue Yen on html
    
    Co-Authored-By: Frederick  <57372810+frederickhalo@users.noreply.github.com>
    Co-Authored-By: kaylasc1 <78955720+kaylasc1@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,16 +1,87 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 import datetime
-app = Flask(__name__)
-@app.route("/")
-def hello():
-   now = datetime.datetime.now()
-   timeString = now.strftime("%Y-%m-%d %H:%M")
-   templateData = {
-      'title' : 'HELLO!',
-      'time': timeString
-   } 
-   # pass in as json dictionary
-   return render_template('index.html', **templateData)
+
+
+class JunkTransporter:
+   def __init__(self):
+      self.weight = 0
+      self.position = [0,0]
+      self.battery = 100
+      self.isOpen = True
+   def move(self):
+      pass
+   def changeDirection(self):
+      pass
+
+   def backToStart(self):
+      pass
    
+   def openTrash(self):
+      pass
+   
+   def closeTrash(self):
+      pass
+
+   def getCurrentWeight(self):
+      return self.weight
+   
+   def updateWeight(self, modifier):
+      self.weight = self.weight - modifier
+      return self.weight
+      
+
+class Response:
+   def __init__(self):
+      self.direction = 0.0 
+      self.numberOfUnits = 0
+
+   def printResponse(self):
+      print("Direction", )
+
+class Sensor:
+   def __init__(self, modelNumber, sensorType):
+      self.modelNumber = modelNumber
+      self.sensorType = sensorType
+
+   def detectObject(self):
+      pass
+
+junkTransporter = JunkTransporter()
+trashSensor = Sensor(1, "detect_trash")
+obstacleSensor = Sensor(2, "detect_obstancle")
+
+# import RPi.GPIO as GPIO
+# import os
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+   # pass in as json dictionary
+   return render_template('index.html')
+
+@app.route('/start_junk_transporter', methods=['GET', 'POST'])
+def startRobot():
+   print("Starting the Junk Transporter!")
+   return "Starting junk transporter"
+
+@app.route('/stop_junk_transporter', methods=['GET', 'POST'])
+def stopRobot():
+   print("Stopping the Junk Transporter!")
+   return "Stopping junk transporter"
+
+@app.route('/open_junk_transporter', methods=['GET', 'POST'])
+def openRobot():
+   print("Opening the Junk Transporter!")
+   return "Opening junk transporter"
+
+@app.route('/close_junk_transporter', methods=['GET', 'POST'])
+def closeRobot():
+   print("Closing the Junk Transporter!")
+   return "Closing junk transporter"
+
+def start():
+   pass
+
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
+
+
